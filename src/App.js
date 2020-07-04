b52aeadc3487812666b4fa96ad628df82a2b05e4
KangJunewoo <brianjune@naver.com>
2020-07-04 23:52:46 +0900

    이게 props구나

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Potato from "./Potato";
+
+// 이걸 es6로 하려면 function Food({fav})로 해도 무관.
+function Food({props}) {
+  return <h1>I like {props.fav}</h1>;
+}
+
 
 // 앱이라는 컴포넌트를 정의하고 있다
 function App() {
   return (
     <div>
       <h1>hello</h1>
-      <Potato/>
+      {/* 여기서 fav라는 props를 전달함. */}
+      <Food fav='kimchi' />
+      <Food fav='ramyeon' />
+      <Food fav='samgyeopsal' />
+      <Food fav='chukumi' />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Potato from './Potato';
 
 // 저 app은 컴포넌트이다.
 // js와 html의 조합 : jsx -> 인접하게 하면 안됨.
