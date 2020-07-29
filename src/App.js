e5ed74d938a0c8b4053175b993ff5f4329391cca
KangJunewoo <brianjune.dev@gmail.com>
2020-07-29 17:29:52 +0900

    [REFACTOR] 복습 & 주석 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// map을 쓸 건데, 이건 배열 각각에 대해 함수를 실행할 수 있게 해줌
-// friends.map((current)=>{
-//   console.log(current); 각각의 값이 콘솔에 찍히고
-//   return 0; 각 연산결과로 array를 만들고 여기 리턴값이 저장됨.
-// }) 이런 식으로!
-
-// 그럼 그 함수 안에 return friend+':)'; 해주면 각각에 미소가 붙은 array를 반환함.
-// 리액트 모든 element들은 달라야 한다는 점 잊지말자. 그래서 id 넣어줌. 실제 쓰이지는 않을거지만!
+// id, name, image, rating으로 된 음식 array
 const foodILike = [
   {
     id: 1,
@@ -46,7 +39,8 @@ const foodILike = [
     rating: 5.0
   }
 ];
-// object destructuring은 es6 문법. 전통적인 방식은 인자에 props넣고 props.name으로 접근하면됨.
+
+// 기존방식 : 인자로 props넣고 props.name으로 접근.
 function Food({ name, image, rating }) {
   return (<div>
     <h2>I like {name}</h2>
@@ -55,7 +49,7 @@ function Food({ name, image, rating }) {
   </div>);
 }
 
-// 대충 prop-types라는 모듈이 필요한데, 몽구스 스키마설정과 비슷한듯.
+// prop-types.PropTypes import 필요.
 // 이 규칙을 어기면 컴파일되긴 하지만 콘솔에 warning이 든다.
 Food.propTypes={
   name:PropTypes.string.isRequired,
@@ -63,12 +57,12 @@ Food.propTypes={
   rating:PropTypes.number
 }
 
-// 모듈화 ㄱㄴ
+// 모듈화.
 function renderFood(dish) {
   return <Food name={dish.name} image={dish.image} rating={dish.rating}/>
 }
 
-// 앱이라는 컴포넌트를 정의하고 있다
+// 앱 컴포넌트 정의.
 function App() {
   return (
     <div>
