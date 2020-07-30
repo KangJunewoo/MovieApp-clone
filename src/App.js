9f75f8cef37926c59b27e43f7b8a413b1b26d15e
KangJunewoo <brianjune.dev@gmail.com>
2020-07-30 17:27:20 +0900

    [#4.2] 태그 입히기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
@@ -11,26 +12,44 @@ class App extends React.Component{
   // 데이터 받는법 : fetch vs axios : 이번엔 axios를 쓰지만 fetch도 알아놓자.
   getMovies = async () => {
     // 순수 데이터는 movies.data.data.movies에 있음. es6문법 총집합이네..
-    const {data:{data:{movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
-    this.setState({movies, isLoading:false});
+    const { data: { data: { movies } } } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
     // this.setState({movies : movies})
   }
-  
+
   // async componentDidMount(){
   //   const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
   // };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getMovies();
   }
 
-  render(){
-    const {isLoading, movies} = this.state;
-    return <div>{isLoading? 'loading...' :movies.map(movie=>{
-      console.log(movie);
-      // 키 줘야 경고안뜸.
-      return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
-    })} </div>
+  render() {
+    const { isLoading, movies } = this.state;
+    return(
+      <section class='container'>
+        {isLoading ?(
+          <div class='loader'>
+            <span class='loader__text'>Loading...</span>
+          </div>
+          ): (
+            <div class="movies">
+              {movies.map(movie=>(
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  poster={movie.medium_cover_image}
+                  title={movie.title}
+                  summary={movie.summary}
+                />
+              ))}
+            </div>
+          )}
+    
+      </section>
+    )
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,19 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import "./Movie.css";
 
 // state가 필요없는 경우 굳이 클래스형이 아니어도 됨. 함수형도 무방.
 function Movie({id, year, title, summary, poster}){
-  return <h4>{title}</h4>
+  return(
+    <div class="movie">
+      <img src={poster} alt={title} title={title}/>
+      <div class="movie_>_data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  )
 }
 
 Movie.propTypes={
